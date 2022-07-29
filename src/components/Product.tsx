5380c8f16a1b5ac21deef6311ceba978106e3919
xtadeushx <alekscherkasov13@gmail.com>
2022-07-29 18:30:15 +0300

    fill: add sort by category

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -15,19 +15,21 @@ const Product = ({ product }: ProductProps) => {
         <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
             <img className="w-1/6" src={product.image} alt={product.title} />
             <p>{product.title}</p>
-            <p className="font-bold">{product.price}</p>
-            <button
-                className={`py-2 px-4 border ${buttonClassName}`}
-                onClick={() => setDetails((prev) => !prev)}>
-                {details ? 'Hide details' : 'Show details'}
-            </button>
+            <p className="font-bold text-gray-500">Price: <span className='text-gray-500  decoration-4 line-through decoration-2' >{product.price}$</span> <span className='text-black'>New price: {(product.price * 0.9).toFixed()}$</span></p>
+
+
 
             {details && (
                 <div className="">
                     <p>{product.description}</p>
-                    <p>Rate: <span style={{fontWeight: 'bold'}}>{product?.rating?.rate}</span></p>
+                    <p>Rate: <span style={{ fontWeight: 'bold' }}>{product?.rating?.rate}</span></p>
                 </div>
-            )}
+            )} 
+             <button
+                className={`py-2 px-4 border ${buttonClassName}`}
+                onClick={() => setDetails((prev) => !prev)}>
+                {details ? 'Hide details' : 'Show details'}
+            </button>
         </div>
     );
 };
